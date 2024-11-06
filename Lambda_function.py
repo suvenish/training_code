0543063e218659198e9d85dece15724835de53b0
alb-binso <145545422+alb-binso@users.noreply.github.com>
2024-11-06 11:02:45 +0530

    Update Lambda_function.py
    Signed-off-by: alb-binso <145545422+alb-binso@users.noreply.github.com>

--- Lambda_function.py
+++ Lambda_function.py
@@ -15,32 +15,32 @@ def get_db_connection():
         user=DB_USER,
         password=DB_PASSWORD,
         database=DB_NAME,
-        cursorclass=pymysql.cursors.DictCursor
-    )
+        cursorclass=pymysql.cursors.DictCursor # Send the commands to db and to retrieve the data from the db
+    ) # in the form of dict
 
 # Lambda handler function
 def lambda_handler(event, context):
     try:
-        operation = event.get('operation')  # "CREATE", "READ", "UPDATE", "DELETE"
+        operation = event.get('operation')  # CRUD
         data = event.get('data', {})
 
         with get_db_connection() as connection:
             with connection.cursor() as cursor:
                 if operation == "CREATE":
-                    sql = "INSERT INTO your_table_name (column1, column2) VALUES (%s, %s)"
-                    cursor.execute(sql, (data['column1'], data['column2']))
-                    connection.commit()
+                    sql = "INSERT INTO your_table_name (col_1, col_2) VALUES (%s, %s)"
+                    cursor.execute(sql, (data['col_1'], data['col_2']))
+                    connection.commit() # To make sure the changes made to the db are consistent
                     return {"status": "Record created successfully"}
 
                 elif operation == "READ":
                     sql = "SELECT * FROM your_table_name WHERE id = %s"
                     cursor.execute(sql, (data['id'],))
-                    result = cursor.fetchone()
+                    result = cursor.fetchone() # Fetch the data from the active db and gives us the fst row
                     return {"status": "Record fetched successfully", "data": result}
 
                 elif operation == "UPDATE":
-                    sql = "UPDATE your_table_name SET column1 = %s WHERE id = %s"
-                    cursor.execute(sql, (data['column1'], data['id']))
+                    sql = "UPDATE your_table_name SET col_1 = %s WHERE id = %s"
+                    cursor.execute(sql, (data['col_1'], data['id']))
                     connection.commit()
                     return {"status": "Record updated successfully"}
 
